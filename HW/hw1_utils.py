7e89dedfcd55f58a8a4485891f47ac735536ae2b
Ben Peloquin <benjamin.peloquin@gmail.com>
2019-04-21 15:37:03 -0700

    finished 1.2.2 variation levels.

--- HW/hw1_utils.py
+++ HW/hw1_utils.py
@@ -4,10 +4,14 @@ Utilities for assignment 1.
 
 """
 
+from collections import defaultdict
 import numpy as np
 import tqdm
 import scipy.stats as stats
-import multiprocessing
+from sklearn.model_selection import train_test_split
+import time
+
+from psych253.metrics import get_confusion_matrix
 
 
 def create_random_split(data, num_trials=None, seed=0):
@@ -125,4 +129,102 @@ def get_reliability_by_variation_level(data, level=0, num_splits=20,
         get_all_neuron_reliabilities(d, num_splits, num_trials, type_)
     return np.mean(reliabilities, axis=1), np.std(reliabilities,
                                                   axis=1), reliabilities
+
+
+def run_models_over_single_split(models, X_train, y_train, X_test, y_test,
+                                 verbose=False):
+    """Run a set of models over a single train/test split.
+
+    Parameters
+    ----------
+    models: list[BaseEsimator]
+        List of sklearn models.
+    X_train: np.array (num_examples, num_features)
+        Training features.
+    y_train: np.array (num_examples, )
+        Training supervision.
+    X_test: np.array (num_examples, num_features)
+        Test features.
+    y_train: np.array (num_examples, )
+        Test supervision.
+    verbose: bool
+        Print current model run.
+
+    Returns
+        tuple (dict, list)
+            First dictionary contains classification results.
+            List contains classifier run-times.
+
+    """
+    create_model_name = lambda x: '{}_{}'.format(x.penalty, x.C)
+    model_names = map(create_model_name, models)
+    labels = set(y_test)
+    # Make sure we don't have any duplicate models
+    assert len(set(model_names)) == len(models)
+
+    results = {}
+    run_times = []
+    for m, n in zip(models, model_names):
+        if verbose:
+            print("Running {} model".format(n))
+        t0 = time.time()
+        m.fit(X_train, y_train)
+        preds = m.predict(X_test)
+        cm = get_confusion_matrix(preds, y_test, labels)
+        run_times.append(time.time() - t0)
+        results[n] = cm
+    return results, run_times
+
+
+def run_models_over_n_splits(models, n_splits, X, y, test_size=0.25,
+                             verbose=True):
+    """Run a set of models of n train/test splits.
+
+    Parameters
+    ----------
+    models: list[BaseEsimator]
+        List of sklearn models.
+    n_splits: int
+        Number of train/test splits to make.
+    X: np.array (num_examples, num_features)
+        Data to split into train/test.
+    y: np.array (num_examples, )
+        Klasses.
+    test_size: float [Default: 0.25]
+        Test split size.
+    random_state: int
+        Random state initializer.
+    verbose: bool
+        Print current model run.
+
+    Returns
+        tuple (dict, dict)
+            First dictionary contains classification results.
+            Second dictionary contains classifier run-times.
+
+    """
+    if verbose:
+        print(
+            "Running {} models over {} splits.".format(len(models), n_splits))
+    results = []
+    times = []
+    pbar = tqdm.tqdm(total=n_splits)
+    for i in range(n_splits):
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=test_size,
+                             random_state=i)
+        res, tim = run_models_over_single_split(models, X_train, y_train,
+                                                X_test, y_test)
+        results.append(res)
+        times.append(tim)
+        if verbose:
+            pbar.update()
+    pbar.close()
+    model_results = defaultdict(list)
+    model_run_times = defaultdict(list)
+    for result, r_times in zip(results, times):
+        for m_name, m_time in zip(result, r_times):
+            model_results[m_name].append(result[m_name])
+            model_run_times[m_name].append(m_time)
+    return model_results, model_run_times
 
